a8f6c5c1736515e19df3569a6d97e2476890b46d
Mr-Ashok <mrashokkumawat6641@gmail.com>
2024-10-16 18:51:36 +0530

    initial commit

---
+++ 03js.js
@@ -0,0 +1,11 @@
+const nme = document.getElementById("name")
+const tagsContainer = document.querySelector(".tags")
+
+const addName = () => {
+    const tag = document.createElement("div")
+    tag.setAttribute("class", "tag")
+
+    tag.innerText = `Hey , I am ${name.value}`;
+
+    tagsContainer.append(tag);
+}
